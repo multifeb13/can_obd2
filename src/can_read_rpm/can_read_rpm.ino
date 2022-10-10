eb246962f36a2f1082d187ae0754e494c33de06c
multifeb13 <musicfair@gmail.com>
2022-10-10 14:46:27 +0900

    [add] definitions

--- src/can_read_rpm/can_read_rpm.ino
+++ src/can_read_rpm/can_read_rpm.ino
@@ -1,6 +1,18 @@
 #include <SPI.h>
 #include <mcp_can.h>
 
+#define STANDARD_CAN_11BIT 1  // That depands on your car. some 1 some 0.
+
+#if STANDARD_CAN_11BIT
+#define CAN_ID_PID 0x7DF
+#else
+#define CAN_ID_PID 0x18db33f1
+#endif
+
+#define PID_ENGIN_PRM 0x0C
+#define PID_VEHICLE_SPEED 0x0D
+#define PID_COOLANT_TEMP 0x05
+
 MCP_CAN CAN(10);
 
 void setup() {
@@ -15,7 +27,14 @@ void setup() {
 void loop() {
     unsigned long id;
     byte len;
-    byte buf[8];
+    byte buf[] = {0x02, 0x01, PID_ENGIN_PRM, 0x00, 0x00, 0x00, 0x00, 0x00};
+#if STANDARD_CAN_11BIT
+    CAN.sendMsgBuf(CAN_ID_PID, 0, 8,
+                   buf);  // ID:0x02, 標準フレーム:0, データ長:8
+#else
+    CAN.sendMsgBuf(CAN_ID_PID, 1, 8,
+                   buf);  // ID:0x02, 標準フレーム:0, データ長:8
+#endif
 
     if (CAN.checkReceive() == CAN_MSGAVAIL) {
         CAN.readMsgBuf(&id, &len, buf);
